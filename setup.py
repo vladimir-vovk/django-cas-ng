1abe02e50beb56def3bdd849d8702f477f168f08
ttyS15 <alex.manaev@gmail.com>
2015-05-29 23:27:59 +0300

    Up to 3.4.2-jw02

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     url='https://github.com/mingchen/django-cas-ng',
     #bugtrack_url='https://github.com/mingchen/django-cas-ng/issues',  # not support this key
     download_url ='https://github.com/mingchen/django-cas-ng/releases',
-    version='3.4.2-jw01',
+    version='3.4.2-jw02',
     install_requires=['Django >= 1.5'],
 )
 
